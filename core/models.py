c2b638750f98353ed0e62efe88dc0bfa2c05f916
dikgobe <djmolepo@gmail.com>
2025-08-21 09:58:01 +0200

    created the sign in Template

--- core/models.py
+++ core/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 
 class Profile(models.Model):
-	#user = models.ForeignKey(User,on_delete = models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	id_user = models.IntegerField()
 	bio = models.TextField(blank = True)
 	profileimg = models.ImageField(upload_to='profile_images', default = 'logo.png')
--- core/views.py
+++ core/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
+
+def signup(request):
+
+	if request.method == 'POST':
+		pass
+	else:
+		pass
+	return render(request, 'signup.html')
